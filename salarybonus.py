9bf0d30209ec18ac260f4d494aeb004c579b0485
vaishnavisuresh588 <vaishnavisuresh588@gami.com>
2025-11-11 11:27:18 +0530

    modified

--- salarybonus.py
+++ salarybonus.py
@@ -14,6 +14,6 @@ bonus = 0.10 * salary
 total_salary = salary + bonus
 
 # Display results
-print(f"Employee Salary: ₹{salary:.2f}")
-print(f"Bonus Amount: ₹{bonus:.2f}")
-print(f"Total Salary after Bonus: ₹{total_salary:.2f}")
+print(f"Employee Salary:{salary:.2f}")
+print(f"Bonus Amount:{bonus:.2f}")
+print(f"Total Salary after Bonus:{total_salary:.2f}")
